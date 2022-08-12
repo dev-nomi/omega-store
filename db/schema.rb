82e376a6d1f2778977a17191b000c623029cbfda
dev-nomi <noman.khalid.freelance@gmail.com>
2022-08-13 00:20:29 +0500

    Enable uuid & future models use uuid by default.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_213528) do
+ActiveRecord::Schema.define(version: 2022_08_12_191729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
